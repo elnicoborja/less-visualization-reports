8975b23d257495ad0b4bcd9e07517a0fc31872fa
Nico Borja <117854200+elnicoborja@users.noreply.github.com>
2025-06-25 14:11:13 -0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -30,4 +30,4 @@ async def upload_file(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn main:app --host=0.0.0.0 --port=8000
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
